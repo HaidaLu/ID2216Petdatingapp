adf09a2cf773fe8e29d75c102489898a7d7972b1
Leroytheone <leroy673412@gmail.com>
2021-02-24 11:53:36 +0300

    finish album

--- app/src/main/java/com/example/petdatingapp/CommunityActivity.java
+++ app/src/main/java/com/example/petdatingapp/CommunityActivity.java
@@ -1,6 +1,16 @@
 package com.example.petdatingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,15 +18,48 @@ import android.view.View;
 
 public class CommunityActivity extends AppCompatActivity {
 
+    private static int RESULT_LOAD_IMAGE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community);
+
+        Button buttonLoadImage = (Button) findViewById(R.id.button3);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+
+//打开本地相册
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                //设定结果返回
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+//获取返回的数据，这里是android自定义的Uri地址
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            //获取选择照片的数据视图
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            //从数据视图中获取已选择图片的路径
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            //将图片显示到界面上
+            ImageView imageView = (ImageView) findViewById(R.id.image_added);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
     }
 
-    public void goToPost(View view) {
-        Intent intent = new Intent(CommunityActivity.this,PostActivity.class);
-        startActivity(intent);
-        return;
-    }
 }
